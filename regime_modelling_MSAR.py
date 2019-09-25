5db4b10e454f06b772ff9bcba817cf3944f431f5
Sonam Srivastava <sonaam1234@gmail.com>
2019-09-25 12:15:40 +0530

    Create regime_modelling_MSAR.py

---
+++ regime_modelling_MSAR.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller
+import matplotlib.pyplot as plt
+
+
+'''
+Get the data and calculate returns
+'''
+nifty = pd.read_csv('nifty.csv', index_col=0, parse_dates=True# Get nifty prices
+nifty_ret = nifty.resample('W').last().pct_change().dropna()# Get weekly returns
+nifty_ret.plot(title='Excess returns', figsize=(12, 3)) # Plot the dataset
+
+
+'''
+Test for stationarity
+'''
+adfuller(nifty_ret.dropna())
+
+
+'''
+Fit the model
+'''
+mod_kns = sm.tsa.MarkovRegression(nifty_ret.dropna(), k_regimes=3, trend='nc', switching_variance=True)
+res_kns = mod_kns.fit()
+res_kns.summary()
+
+
+'''
+Plot smoothed probabilities
+'''
+fig, axes = plt.subplots(3, figsize=(10,7))ax = axes[0]
+ax.plot(res_kns.smoothed_marginal_probabilities[0])
+ax.set(title='Smoothed probability of a low-variance regime for stock returns')ax = axes[1]
+ax.plot(res_kns.smoothed_marginal_probabilities[1])
+ax.set(title='Smoothed probability of a medium-variance regime for stock returns')ax = axes[2]
+ax.plot(res_kns.smoothed_marginal_probabilities[2])
+ax.set(title='Smoothed probability of a high-variance regime for stock returns')fig.tight_layout()
